4fa66ba9d0a3e7f639d989cd17cf82aa805783fc
jl <vctrowino@yahoo.com>
2013-08-03 08:36:01 -0500

    draft-05

--- coas/src/cs/mum/mb/Helper.java
+++ coas/src/cs/mum/mb/Helper.java
@@ -77,7 +77,7 @@ message.setText(vmsg);
 // Send message
 //Transport.send(message);
 Transport transport = mailSession.getTransport("smtp");
-transport.connect( null,from,"manyamamanyonyi");
+transport.connect( null,from,"password");
 transport.sendMessage(message,message.getAllRecipients());
 //result = "Sent message successfully....v";
 }catch (MessagingException mex) {
