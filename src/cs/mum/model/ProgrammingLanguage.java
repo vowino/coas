f1efb0494697dc6bdf93df33c09cd3f55245375e
email <okeyobiajunwa@gmail.com>
2013-08-05 23:04:59 -0500

    some changes

--- src/cs/mum/model/ProgrammingLanguage.java
+++ src/cs/mum/model/ProgrammingLanguage.java
@@ -1,5 +1,16 @@
 package cs.mum.model;
 
 public class ProgrammingLanguage {
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
 
 }
